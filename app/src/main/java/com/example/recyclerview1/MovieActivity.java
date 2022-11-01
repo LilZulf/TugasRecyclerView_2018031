a9880ddc2ceb50abab1f27c7ffafb0ec464ad7ea
Najib Zulfan <najibzulfan@gmail.com>
2022-11-01 23:37:02 +0700

    (+) Adding Staggered and Grid layout manager

--- app/src/main/java/com/example/recyclerview1/MovieActivity.java
+++ app/src/main/java/com/example/recyclerview1/MovieActivity.java
@@ -2,11 +2,15 @@ package com.example.recyclerview1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.recyclerview1.adapters.FoodAdapter;
@@ -21,6 +25,7 @@ import java.util.List;
 
 public class MovieActivity extends AppCompatActivity {
     RecyclerView rvMovie;
+    Button linear,grid,staggered;
     public List<Movie> movieList = new ArrayList<>();
 
     @Override
@@ -28,6 +33,9 @@ public class MovieActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
         setTitle("Select your movie");
+        linear = findViewById(R.id.btnLinear);
+        grid = findViewById(R.id.btnGrid);
+        staggered = findViewById(R.id.btnStaggered);
 
         rvMovie = findViewById(R.id.rv_movie);
 
@@ -35,10 +43,30 @@ public class MovieActivity extends AppCompatActivity {
         MovieAdapter movieAdapter = new MovieAdapter(movieList);
 
         rvMovie.setAdapter(movieAdapter);
-        rvMovie.setLayoutManager(new LinearLayoutManager(this));
+        rvMovie.setLayoutManager(new LinearLayoutManager(MovieActivity.this));
 
-        rvMovie.addItemDecoration(new DividerItemDecoration
-                (this, DividerItemDecoration.VERTICAL));
+
+
+        linear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rvMovie.setLayoutManager(new LinearLayoutManager(MovieActivity.this));
+            }
+        });
+        staggered.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL);
+                rvMovie.setLayoutManager(staggeredGridLayoutManager);
+            }
+        });
+        grid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GridLayoutManager gridLayoutManager = new GridLayoutManager(getApplicationContext(),3,LinearLayoutManager.VERTICAL,false);
+                rvMovie.setLayoutManager(gridLayoutManager); // set LayoutManager to RecyclerView
+            }
+        });
 
     }
 
